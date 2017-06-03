9887e127a2862248f44ddd263d680c90231d79f9
amirkiperwas <samirkip@gmail.com>
2017-06-04 00:21:04 +0300

    Removed real from results, support odd sqrtN

--- SPURS_DEMO/ServiceFunctions/IFFTandFilterInImageSpace.m
+++ SPURS_DEMO/ServiceFunctions/IFFTandFilterInImageSpace.m
@@ -1,14 +1,17 @@
 function [uniform_k_samples, OutputImage] = IFFTandFilterInImageSpace(c,ImgFilter,sqrtN,OverGridFactor)
 
-FOV_idx = ((sqrtN*OverGridFactor)/2-sqrtN/2+1:(sqrtN*OverGridFactor)/2+sqrtN/2);
-
+if mod(sqrtN,2)==0
+    FOV_idx = round((sqrtN*OverGridFactor)/2-sqrtN/2+1:(sqrtN*OverGridFactor)/2+sqrtN/2);
+else    
+    FOV_idx = (ceil((sqrtN*OverGridFactor)/2-sqrtN/2)+1:ceil((sqrtN*OverGridFactor)/2+sqrtN/2));
+end
 IFFT_c = ifftshift(ifft2(fftshift(c)));
 OutputImage = (sqrtN*OverGridFactor)^2.*IFFT_c.*abs(ImgFilter);
 
 %calculate k-space samples on the uniform grid
-uniform_k_samples = ifftshift(fft2(fftshift(real(OutputImage))));
+uniform_k_samples = ifftshift(fft2(fftshift(OutputImage)));
 
 % Crop to FOV and returen real Image
-OutputImage = real(OutputImage(FOV_idx,FOV_idx)); 
+OutputImage = OutputImage(FOV_idx,FOV_idx); 
 end
 
